3693a72c250eb4a259d3b4b4ae16caa05ffa2f21
greyewi1 <greyewi@gmail.com>
2021-12-11 16:25:13 +0500

    indexed files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LetterBuilder from './LetterBuilder'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LetterBuilder/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
